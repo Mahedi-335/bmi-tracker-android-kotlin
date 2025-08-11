3534dc368f61f60c43e16c7c2f8c097e434c5e31
Mahedi-335 <mdmahedihasan335@gmail.com>
2025-08-12 01:41:48 +0600

    Fixed Welcome Screen and Get User Name in Main Screen

--- app/src/main/java/com/example/bodybalance/MainActivity.kt
+++ app/src/main/java/com/example/bodybalance/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.bodybalance
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val nameText = findViewById<TextView>(R.id.nameTextView)
+        val btnMale = findViewById<MaterialButton>(R.id.btnMale)
+        val btnFemale = findViewById<MaterialButton>(R.id.btnFemale)
+        val ageEditText = findViewById<EditText>(R.id.ageInput)
+        val weightEditText = findViewById<EditText>(R.id.weightInput)
+        val heightEditText = findViewById<EditText>(R.id.heightInput)
+        val calculateButton = findViewById<AppCompatButton>(R.id.calculateButton)
+
+        val name = intent.getStringExtra("USER_NAME") ?: ""
+        if (name.isNotEmpty()){
+            nameText.text = "Hi $name!"
+        }else{
+            nameText.text = "Hi!"
         }
+
     }
 }
--- app/src/main/java/com/example/bodybalance/WelcomeActivity.kt
+++ app/src/main/java/com/example/bodybalance/WelcomeActivity.kt
@@ -2,7 +2,9 @@ package com.example.bodybalance
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputFilter
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,9 +19,22 @@ class WelcomeActivity : AppCompatActivity() {
         val inputEditText = findViewById<EditText>(R.id.inputEditText)
         val nextButton = findViewById<MaterialButton>(R.id.nextButton)
 
+        inputEditText.filters = arrayOf(InputFilter.LengthFilter(12))
+
         nextButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+
+            val name = inputEditText.text.toString()
+
+            if (name.isEmpty()){
+                Toast.makeText(this, "Enter your name!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val intent = Intent(this, MainActivity::class.java).apply {
+                putExtra("USER_NAME", name)
+            }
             startActivity(intent)
+            finish()
+
         }
 
     }
