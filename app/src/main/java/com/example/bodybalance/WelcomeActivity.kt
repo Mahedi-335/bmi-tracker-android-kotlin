c023dc08c2e6400f4624607555aa9d187aa26a55
Mahedi-335 <mdmahedihasan335@gmail.com>
2025-08-11 17:47:33 +0600

    Add intent

--- app/src/main/java/com/example/bodybalance/WelcomeActivity.kt
+++ app/src/main/java/com/example/bodybalance/WelcomeActivity.kt
@@ -1,15 +1,26 @@
 package com.example.bodybalance
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val nextButton = findViewById<MaterialButton>(R.id.nextButton)
+
+        nextButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
