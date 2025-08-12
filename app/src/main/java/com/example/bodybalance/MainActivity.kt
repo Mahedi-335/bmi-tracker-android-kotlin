455a8b5f5af6d1792dbb196048b8e81485464c26
Mahedi-335 <mdmahedihasan335@gmail.com>
2025-08-12 23:34:27 +0600

    Adding Spinner for Weight

--- app/src/main/java/com/example/bodybalance/MainActivity.kt
+++ app/src/main/java/com/example/bodybalance/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.bodybalance
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +14,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.button.MaterialButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,13 +26,32 @@ class MainActivity : AppCompatActivity() {
         val weightEditText = findViewById<EditText>(R.id.weightInput)
         val heightEditText = findViewById<EditText>(R.id.heightInput)
         val calculateButton = findViewById<AppCompatButton>(R.id.calculateButton)
+        val weightSpinner = findViewById<Spinner>(R.id.weightSpinner)
 
+        // Name Show
         val name = intent.getStringExtra("USER_NAME") ?: ""
         if (name.isNotEmpty()){
             nameText.text = "Hi $name!"
         }else{
             nameText.text = "Hi!"
         }
+
+        // Weight Spinner Setup
+        val units = resources.getStringArray(R.array.weight_units)
+        val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,units)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        weightSpinner.adapter = adapter
+
+        // Listener
+        weightSpinner.onItemSelectedListener = this
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long){
+        val selectedUnit = parent.getItemAtPosition(position).toString()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
 
     }
 }
